3ff3d4d6ae0c38e08e767d8a4ce662fc34e7a89a
danielveg4 <dvgilles@gmail.com>
2024-10-01 14:28:19 +0200

    Update script.js

--- tipeCalculator/script.js
+++ tipeCalculator/script.js
@@ -1,13 +1,12 @@
-const amountUsersInputElement = document.getElementById('user-import');
+const billInputElement = document.getElementById('user-import');
 const buttonTipsContainerElement = document.getElementById('buttons-tip');
 const numberUsersInputElement = document.getElementById('users');
 const resultTipPersonElement = document.getElementById('tip-person');
 const resultTotalPersonElement = document.getElementById('total-person');
 const formElement = document.getElementById('tip-calculator');
 
-let tipSelected = '';
-let tipPerPersonImport = '';
-let totalPerPersonImport = '';
+let tipSelected = 0;
+
 
 const isChecked = () => {
     const allCheckbox = buttonTipsContainerElement.children;
@@ -21,7 +20,7 @@ const isChecked = () => {
     if (checkboxCount > 1) {
         console.log('seleccionada más de una opcion!');
         return;
-    }
+    } 
     if (allCheckbox[allCheckbox.length - 1].value !== '') {
         tipSelected = allCheckbox[allCheckbox.length - 1].value / 100;
     }
@@ -29,16 +28,17 @@ const isChecked = () => {
 }
 
 const tipOperation = () => {
-    const totalBill = amountUsersInputElement.value;
-    const totalUsers = numberUsersInputElement.value;
+    const totalBill = parseFloat(billInputElement.value);
+    const totalUsers = parseInt(numberUsersInputElement.value);
     const tipImport = (totalBill * tipSelected);
-    tipPerPersonImport = (tipImport / totalUsers);
-    totalPerPersonImport = ((totalBill / totalUsers) + tipPerPersonImport);
+
+    const tipPerPersonImport = (tipImport / totalUsers);
+    const totalPerPersonImport = ((totalBill / totalUsers) + tipPerPersonImport);
   
-    printAmounts()
+    printAmounts(tipPerPersonImport, totalPerPersonImport)
 }
 
-const printAmounts = () => {
+const printAmounts = (tipPerPersonImport, totalPerPersonImport) => {
     resultTipPersonElement.textContent = `€${tipPerPersonImport.toFixed(2)}`;
     resultTotalPersonElement.textContent = `€${totalPerPersonImport.toFixed(2)}`
 }
